b6ee77f39687d44d60cbe80d4b312f1251d84d33
Flee-Time <koray509@gmail.com>
2025-04-01 16:00:34 +0300

    updated projects

--- pages/projects.js
+++ pages/projects.js
@@ -3,10 +3,11 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 
+import thumbPiNas from '../public/images/projects/pinas.png'
 import thumbSlimeVR from '../public/images/projects/slimevr.png'
 import thumbDireksiyon from '../public/images/projects/direksiyon.png'
 import thumb3DPrinter from '../public/images/projects/3dprinter.png'
-import thumbFlLight from '../public/images/projects/fllight.png'
+import thumbFlLight from '../public/images/projects/freqbox.png'
 import thumbVerlet from '../public/images/projects/verlet.png'
 import thumbArdupilot from '../public/images/projects/ardupilot.png'
 import thumbCDworldgen from '../public/images/projects/cdworldgen.png'
@@ -19,38 +20,43 @@ const Projects = () => (
       </Heading>
         <SimpleGrid columns={[1, 1, 2]} gap={6}>
             <Section>
+              <GridItem id="PiNas" title="Raspberry Pi 5 NAS" thumbnail={thumbPiNas}>
+              Bu NAS sistemi, Raspberry Pi 5 üzerinde çalışmakta olup, aynı anda 5 adet HDD'ye bağlanabilir. Jellyfin medya sunucusu, Immich fotoğraf yönetim aracı ve SMB paylaşımları ile entegre edilmiştir. Bu yapı, medya içeriğinizi verimli bir şekilde depolayabilir ve ağ üzerinden kolayca erişim sağlayabilir. Sistemin çerçevesi, tasarladığım 3D baskılı bağlantı elemanları ve alüminyum profiller kullanılarak kendim yapılmıştır. Düşük enerji tüketimiyle yüksek performans sunarak ev veya ofis kullanımı için ideal bir çözüm sağlar.
+              </GridItem>
+            </Section>
+            <Section>
               <GridItem id="SlimeVR" title="Sanal Gerçeklikte Ayak Takibi" thumbnail={thumbSlimeVR}>
-                Sanal gerçeklikte normalde sadece ellerimiz ve kafamız takip edilir. Benim tasarladığım kart ile ucuz maliyetle ayaklarınızı takip ettirebilirisiniz.
+                Sanal gerçeklik sistemlerinde genellikle yalnızca eller ve kafa takip edilir. Ancak benim tasarladığım özel PCB ile, düşük maliyetle ayak takibini mümkün kılarak sanal gerçeklik deneyiminizi geliştirmenize olanak sağlar.
               </GridItem>
             </Section>
             <Section>
               <GridItem id="Direksiyon" title="900 Derecelik Simulatör Direksiyon" thumbnail={thumbDireksiyon}>
-                Şuanda piyasada olan profesyonel direksiyonlar 5000TL'den başlıyor. Ben toplam 786TL maliyet çıkararak kendime 900 derece olan aşırı hassas bir direksiyon yapmış oldum. Çoğu parçası üç boyutlu printerdan basılmıştır.
+                Piyasadaki profesyonel direksiyon setleri genellikle 5000 TL'den başlamakta. Ben ise yalnızca 786 TL maliyetle, 900 derece dönebilen son derece hassas bir direksiyon tasarladım. Direksiyonun çoğu parçası, 3D yazıcı kullanılarak üretilmiştir.
               </GridItem>
             </Section>
             <Section>
-              <GridItem id="3DPrinter" title="3D Printer" thumbnail={thumb3DPrinter}>
-                2300TL'lik parçalar ile eski üç boyutlu printerımı kullanarak yeni birtane printer üretmiş oldum. Üç boyutlu basılmış olan parcaların hepsi benim printerımdan basılmıştır.
+              <GridItem id="3DPrinter" title="Kendi Yaptığım 3D Yazıcı" thumbnail={thumb3DPrinter}>
+                Eski bir 3D yazıcıyı kullanarak, 2300 TL değerinde malzeme ile tamamen yeni bir yazıcı inşa ettim. Yazıcının tüm 3D basılmış parçaları, kendi üretimim olan yazıcımdan basılmıştır.
               </GridItem>
             </Section>
             <Section>
-              <GridItem id="FlLight" title="Flipper Zero'nun el yapımı klonu" thumbnail={thumbFlLight}>
-                Flipper Zero adıyla tanınan hackleme cihazının sadece subghz özelliği olan klonudur. Yazılan bütün kodları bana aittir, Tamamen C dilinde yazılmıştır.
+              <GridItem id="FlLight" title="FreqBox" thumbnail={thumbFlLight}>
+                Bu cihaz, Flipper Zero adıyla bilinen hackleme cihazının yalnızca SubGHz özellikli klonudur. Tüm yazılım kodları tamamen bana aittir ve C dilinde yazılmıştır. Ayrıca, PCB tasarımı da tamamen bana ait ve özelleştirilmiştir.
               </GridItem>
             </Section>
             <Section>
-              <GridItem id="Verlet" title="Verlet formülü kullanan fizik simulatörü" thumbnail={thumbVerlet}>
-                Verlet fizik formülü ile calışan küçük bir fizik simulatörüdür. C++ kullanılarak yazılmıştır. Github'ımdan derleyip inceleyebilirsiniz.
+              <GridItem id="Verlet" title="Verlet Fizik Simülatörü" thumbnail={thumbVerlet}>
+                Bu, Verlet fizik formülüyle çalışan küçük bir fizik simülatörüdür. C++ kullanılarak geliştirilmiş olup, kaynak kodlarına GitHub üzerinden ulaşabilir ve derleyerek inceleyebilirsiniz..
               </GridItem>
             </Section>
             <Section>
               <GridItem id="ardupilot" title="Ardupilot yazılımını ESP32'de çalıştırmak" thumbnail={thumbArdupilot}>
-                Ardupilot bir uçuş kontorl kart yazılımıdır. Normalde 3000TL olan kontrolcülere yuklenebilen bir yazılımdır. Ben ve birsürü kişi Ardupilot'ı ESP32 ye portlamayı başardık. ESP32 200TL'lik bir karttır ve birkaç modül ekleyerek yazdığımız kodu çalıştırabilirsini. Bu port Teknofest'te yarışan Kraken takımı için yapılmıştır. Koda Github'ta bakabilirsiniz.
+                Ardupilot, genellikle 3000 TL'lik uçuş kontrol kartlarına yüklenebilen bir uçuş kontrol yazılımıdır. Ben ve bir grup geliştirici, Ardupilot’ı ESP32 kartına portlamayı başardık. ESP32, 200 TL değerinde bir kart olup, birkaç modül ekleyerek yazılımı çalıştırmak mümkündür. Bu port, Teknofest'te yarışan Kraken takımı için yapılmıştır. Koda GitHub üzerinden ulaşabilirsiniz.
               </GridItem>
             </Section>
             <Section>
               <GridItem id="cdworldgen" title="Minecraft Crafting Dead, Ek yapı modu " thumbnail={thumbCDworldgen}>
-                Minecraft için olan Crafting Dead mod paketi tek oyunculu yada özel serverlerde yapılar olmadığından oyanamıyordu, arkadaşlarımla oynamak istediğimden kendi yapı modumu kodladım. Github'da koda bakıp modun kendisini indirebilirsiniz, Mod Java dilinde kodlanmıştır.
+                Minecraft'ta Crafting Dead mod paketinin, tek oyunculu ya da özel sunucularda yapı eksikliği nedeniyle oynanabilirliği sınırlıydı. Arkadaşlarımla birlikte oynamak istediğim için, kendi yapı modumu geliştirdim. Mod, Java dilinde yazılmıştır ve GitHub'dan hem kaynak koda hem de mod dosyasına erişebilirsiniz.
               </GridItem>
             </Section>
         </SimpleGrid>
